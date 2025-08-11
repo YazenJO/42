0d22b2044d97423f1343751d16fd70a0f1e8684a
YazenJO <yazenbilal2005@gmail.com>
2025-08-11 23:34:07 +0300

    ft_memcmp

--- Core/libft/ft_lstiter.c
+++ Core/libft/ft_lstiter.c
@@ -6,7 +6,7 @@
 /*   By: marvin <marvin@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/08/10 21:32:37 by marvin            #+#    #+#             */
-/*   Updated: 2025/08/10 22:05:43 by marvin           ###   ########.fr       */
+/*   Updated: 2025/08/10 22:10:13 by marvin           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,3 +22,22 @@ void	ft_lstiter(t_list *lst, void (*f)(void *))
 		lst = lst->next;
 	}
 }
+
+/*
+# include <stdio.h>
+
+void	print_content(void *content)
+{
+	printf("%s\n", (char *)content);
+}
+
+int	main(void)
+{
+	t_list	*list;
+
+	list = ft_lstnew("Hello");
+	ft_lstadd_back(&list, ft_lstnew("World"));
+	ft_lstiter(list, print_content);
+	return (0);
+}
+*/
--- Core/libft/ft_lstlast.c
+++ Core/libft/ft_lstlast.c
@@ -6,7 +6,7 @@
 /*   By: marvin <marvin@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/08/10 21:02:10 by marvin            #+#    #+#             */
-/*   Updated: 2025/08/10 22:05:47 by marvin           ###   ########.fr       */
+/*   Updated: 2025/08/10 22:13:02 by marvin           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,3 +20,19 @@ t_list	*ft_lstlast(t_list *lst)
 		lst = lst->next;
 	return (lst);
 }
+
+/*
+#include <stdio.h>
+int main(void)
+{
+	t_list *list;
+	t_list *last;
+
+	list = ft_lstnew("Hello");
+	ft_lstadd_back(&list, ft_lstnew("World"));
+	last = ft_lstlast(list);
+	if (last)
+		printf("Last element: %s\n", (char *)last->content);
+	return (0);
+}
+*/
